fb7da6187615e9e4f40c639b9c010e4d72dcf863
Sergio <sergio.skate89@gmail.com>
2023-04-06 21:05:26 -0600

    Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FeatureModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,11 +11,15 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   trendingMovies: any;
+  theatreMovies: any;
+  popularMovies: any;
 
-  constructor (private http: HttpClient) { }
+  constructor (private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.getTrendingMovies();
+    this.getTheatreMovies();
+    this.getPopularMovies();
   }
 
   getTrendingMovies() {
@@ -23,6 +27,24 @@ export class HomeComponent implements OnInit {
       this.trendingMovies = movies;
       console.log(this.trendingMovies)
     });
+  }
+
+  getTheatreMovies() {
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe((movies) => {
+      this.theatreMovies = movies;
+      console.log(this.theatreMovies)
+    });
+  }
+
+  getPopularMovies() {
+    this.http.get('http://localhost:4200/assets/data/popular-movies.json').subscribe((movies) => {
+      this.popularMovies = movies;
+      console.log(this.popularMovies)
+    });
+  }
+
+  goToMovie(type: string, id: string) {
+    this.router.navigate(['movie', type, id]);
   }
 
 }
